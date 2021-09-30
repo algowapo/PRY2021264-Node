a88574bfc914197bff9f84953659d95e0ea42c21
TheRoro <39977138+TheRoro@users.noreply.github.com>
2021-09-30 14:38:28 -0500

    🔒 Add CORS

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 
 const express = require('express')
 const app = express()
+var cors = require('cors')
 const mongoose = require('mongoose')
 
 mongoose.connect(process.env.DATABASE_URL)
@@ -9,6 +10,11 @@ const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to database'))
 
+var corsOptions = {
+	origin: '*',
+}
+
+app.use(cors(corsOptions))
 app.use(express.json())
 
 const usersRouter = require('./routes/users')

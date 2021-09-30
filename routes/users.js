fd98bae8619bb85c379c6bf58639fb1bd377cc39
Rodrigo Ramirez <rodrigo8rb@gmail.com>
2021-09-29 22:12:35 -0500

    ⚡️Finish User Validation
    Add validation on user creation and login.

--- routes/users.js
+++ routes/users.js
@@ -20,11 +20,15 @@ router.post('/', async (req, res) => {
 		username: req.body.username,
 		password: req.body.password,
 	})
-	try {
-		const newUser = await user.save()
-		res.status(201).json(newUser)
-	} catch (e) {
-		res.status(400).json({ message: e.message })
+	if (!(await userAlreadyExists(user))) {
+		try {
+			const newUser = await user.save()
+			res.status(201).json(newUser)
+		} catch (e) {
+			res.status(400).json({ message: e.message })
+		}
+	} else {
+		res.status(403).json({ message: 'User already exists' })
 	}
 })
 // Update One
@@ -47,7 +51,14 @@ router.delete('/:id', getUser, async (req, res) => {
 	try {
 		await res.user.remove()
 		res.json({ message: 'User deleted' })
-	} catch (e) {}
+	} catch (e) {
+		res.status(400).json({ message: e.message })
+	}
+})
+
+// Login
+router.post('/login', login, async (req, res) => {
+	res.json(res.user)
 })
 
 async function getUser(req, res, next) {
@@ -56,6 +67,36 @@ async function getUser(req, res, next) {
 		user = await UserModel.findById(req.params.id)
 		if (user == null) {
 			return res.status(404).json({ message: 'Cannot find user' })
+		}
+	} catch (e) {
+		return res.status(500).json({ message: e.message })
+	}
+	res.user = user
+	next()
+}
+
+async function userAlreadyExists(newUser) {
+	let user
+	try {
+		user = await UserModel.findOne({ username: newUser.username })
+		if (!user) {
+			return false
+		}
+		return true
+	} catch (e) {
+		return true
+	}
+}
+
+async function login(req, res, next) {
+	let user
+	try {
+		user = await UserModel.findOne({ username: req.body.username })
+		if (!user) {
+			return res.status(404).json({ message: 'Wrong username' })
+		}
+		if (user.password != req.body.password) {
+			return res.status(401).json({ message: 'Wrong password' })
 		}
 	} catch (e) {
 		return res.status(500).json({ message: e.message })
